8c6d48259a22f75f64cf8ddd4c7804cdfc7b013c
yasminnabila <yasmnnabila@gmail.com>
2022-12-18 01:20:52 +0700

    Create local state in app for notes

--- react-notes-app/src/App.js
+++ react-notes-app/src/App.js
@@ -1,6 +1,31 @@
+import { useState } from "react";
+import { nanoid } from "nanoid";
 import NotesList from "./components/NotesList";
 
 const App = () => {
+  const [notes, setNotes] = useState([
+    {
+      id: nanoid(),
+      text: "This is my first note!",
+      date: "18/12/22",
+    },
+    {
+      id: nanoid(),
+      text: "This is my second note!",
+      date: "18/12/22",
+    },
+    {
+      id: nanoid(),
+      text: "This is my third note!",
+      date: "18/12/22",
+    },
+    {
+      id: nanoid(),
+      text: "This is my fourth note!",
+      date: "18/12/22",
+    },
+  ]);
+
   return (
     <div className="container">
       <NotesList />
