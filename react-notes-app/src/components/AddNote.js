95caeef6bfe604913d4c5329676c73314de86c97
yasminnabila <yasmnnabila@gmail.com>
2022-12-20 11:05:27 +0700

    Create state for textarea

--- react-notes-app/src/components/AddNote.js
+++ react-notes-app/src/components/AddNote.js
@@ -1,10 +1,18 @@
+import { useState } from "react";
+
 const AddNote = () => {
+  const [noteText, setNoteText] = useState("");
+  const handleChange = (event) => {
+    setNoteText(event.target.value);
+  };
   return (
     <div className="note new">
       <textarea
         rows="8"
         cols="10"
         placeholder="Type to add a note..."
+        value={noteText}
+        onChange={handleChange}
       ></textarea>
       <div className="note-footer">
         <small>200 remaining</small>
