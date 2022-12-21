dce7e2f044124333e5a1abf6f6ee60052166d301
yasminnabila <yasmnnabila@gmail.com>
2022-12-21 20:00:00 +0700

    Add character limit variable

--- react-notes-app/src/components/AddNote.js
+++ react-notes-app/src/components/AddNote.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 const AddNote = ({ handleAddNote }) => {
   const [noteText, setNoteText] = useState("");
+  const characterLimit = 200;
 
   const handleChange = (event) => {
     setNoteText(event.target.value);
