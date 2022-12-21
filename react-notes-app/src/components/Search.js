81511cca3696503bc473b1e4153b14d4a333bd7a
yasminnabila <yasmnnabila@gmail.com>
2022-12-21 22:02:11 +0700

    Add search icon and input type

--- react-notes-app/src/components/Search.js
+++ react-notes-app/src/components/Search.js
@@ -1,5 +1,12 @@
+import { MdSearch } from "react-icons/md";
+
 const Search = () => {
-  return <div></div>;
+  return (
+    <div className="search">
+      <MdSearch className="search-icons" size="1.3em" />
+      <input type="text" placeholder="Type to search..." />
+    </div>
+  );
 };
 
 export default Search;
